7e409754bd6da2b90e6a3fbdc134647efdbc6d6c
durbanoutie <durbanout13@gmail.com>
2024-06-01 16:50:09 +0200

    update build script
    - added jar'ing to the build process
    - added latest distributable for easy use elsewhere

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
-
+echo "Cleaning build directory..."
 rm -rf ./build
 mkdir ./build
 mkdir ./build/classes
 #mkdir ./build/resources
 
-#cp -r ./src/main/resources ./build/
+echo "compiling library sources..."
+srcs=`find ./src/main/java -not -type d -not -name Main.*`
+echo $srcs
+javac -d ./build/classes $srcs
 
-cpath=./build/classes:\
-./src/main/java/
+echo "packaging library..."
+bins=`find ./build/classes -not -type d`
+jar cf ./dist/sira.jar -C ./build/classes sira4j/Sira.class $bins
 
-javac -cp $cpath -d ./build/classes ./src/main/java/Main.java
+echo "building sample project..."
+javac -cp ./dist/sira.jar -d ./build/classes ./src/main/java/Main.java
+
